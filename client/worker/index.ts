382e307d066d987494025d3dc2d0332c191162ab
Sk Mijanur Rahaman <mijanur.microace@gmail.com>
2026-07-31 15:50:54 +0530

    feat: implement service worker for push notifications and notification click handling

--- client/worker/index.ts
+++ client/worker/index.ts
@@ -1,4 +1,4 @@
-self.addEventListener('push', function (event) {
+self.addEventListener('push', function (event: any) {
   if (event.data) {
     const data = event.data.json();
     const options = {
@@ -16,7 +16,7 @@ self.addEventListener('push', function (event) {
   }
 });
 
-self.addEventListener('notificationclick', function (event) {
+self.addEventListener('notificationclick', function (event: any) {
   event.notification.close();
   if (event.notification.data && event.notification.data.url) {
     event.waitUntil(clients.openWindow(event.notification.data.url));
